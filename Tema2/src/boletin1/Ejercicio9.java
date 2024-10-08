cbda1f927d393d1f1cf737f8fb0da35a7613b220
ivan.carrascosa <ivan.carrascosa@106-06.info2>
2024-10-08 14:36:20 +0200

    Actualización ejercicio 9

--- Tema2/src/boletin1/Ejercicio9.java
+++ Tema2/src/boletin1/Ejercicio9.java
@@ -8,6 +8,10 @@ public class Ejercicio9 {
 		// Creo las variables para guardar la elección de los jugadores
 		String jug1;
 		String jug2;
+		//Creo constantes para las diferentes opciones
+		final String PIEDRA = "PIEDRA";
+		final String PAPEL = "PAPEL";
+		final String TIJERA = "TIJERA";
 		// Creo el escáner
 		Scanner reader = new Scanner(System.in);
 		// Pido a los jugadores que introduzcan su elección y lo guardo en las variables
@@ -18,17 +22,17 @@ public class Ejercicio9 {
 		// Voy comparando y creo las diferentes situaciones
 		if (jug1.equals(jug2)) {
 			System.out.println("Habéis empatado");
-		} else if (jug1.equals("PIEDRA") && jug2.equals("TIJERA")) {
+		} else if (jug1.equals(PIEDRA) && jug2.equals(TIJERA)) {
 			System.out.println("Gana el jugador 1");
-		} else if (jug1.equals("PIEDRA") && jug2.equals("PAPEL")) {
+		} else if (jug1.equals(PIEDRA) && jug2.equals(PAPEL)) {
 			System.out.println("Gana el jugador 2");
-		} else if (jug1.equals("PAPEL") && jug2.equals("PIEDRA")) {
+		} else if (jug1.equals(PAPEL) && jug2.equals(PIEDRA)) {
 			System.out.println("Gana el jugador 1");
-		} else if (jug1.equals("PAPEL") && jug2.equals("TIJERA")) {
+		} else if (jug1.equals(PAPEL) && jug2.equals(TIJERA)) {
 			System.out.println("Gana el jugador 2");
-		} else if (jug1.equals("TIJERA") && jug2.equals("PAPEL")) {
+		} else if (jug1.equals(TIJERA) && jug2.equals(PAPEL)) {
 			System.out.println("Gana el jugador 1");
-		} else if (jug1.equals("TIJERA") && jug2.equals("PIEDRA")) {
+		} else if (jug1.equals(TIJERA) && jug2.equals(PIEDRA)) {
 			System.out.println("Gana el jugador 2");
 		}
 
