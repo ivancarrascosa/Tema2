22e4010088e29a2b1dfbcdd74fc3501cdd2c7167
ivan.carrascosa <ivan.carrascosa@106-06.info2>
2024-10-14 11:50:13 +0200

    Actualizacion ejercicio 8

--- Tema2/src/boletin3/Ejercicio8.java
+++ Tema2/src/boletin3/Ejercicio8.java
@@ -16,26 +16,17 @@ public class Ejercicio8 {
 		System.out.println("Introduzca el año");
 		año = reader.nextInt();
 		// Distinguimos el caso de que el año sea bisiesto y el mes sea febrero, en cuyo
-		// caso serán 29 días, hacemos el resto con un switch
-		if ((año%4 == 0) && (mes == 2)) {
+		// caso serán 29 días, hacemos el resto con un switch ya que no dependen del año
+		if ((año % 4 == 0) && (mes == 2)) {
 			System.out.println("El mes tiene 29 días");
 		} else {
 			switch (mes) {
-			case 1 -> System.out.println("El mes tiene 31 días");
+			case 1, 3, 5, 7, 8, 10, 12 -> System.out.println("El mes tiene 31 días");
 			case 2 -> System.out.println("El mes tiene 28 días");
-			case 3 -> System.out.println("El mes tiene 31 días");
-			case 4 -> System.out.println("El mes tiene 30 días");
-			case 5 -> System.out.println("El mes tiene 31 días");
-			case 6 -> System.out.println("El mes tiene 30 días");
-			case 7 -> System.out.println("El mes tiene 31 días");
-			case 8 -> System.out.println("El mes tiene 31 días");
-			case 9 -> System.out.println("El mes tiene 30 días");
-			case 10 -> System.out.println("El mes tiene 31 días");
-			case 11 -> System.out.println("El mes tiene 30 días");
-			case 12 -> System.out.println("El mes tiene 31 días");
+			case 4, 6, 9, 11 -> System.out.println("El mes tiene 30 días");
 			}
 		}
-		//Cerramos el escáner
+		// Cerramos el escáner
 		reader.close();
 	}
 
