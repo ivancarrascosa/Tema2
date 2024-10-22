ea16b15dbbdc19dc8eaf7441e654f227feda5432
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2024-10-22 14:39:34 +0200

    Ejercicios 1 y 2 del boletin 6

---
+++ Tema2/src/boletin6/Ejercicio1.java
@@ -0,0 +1,23 @@
+package boletin6;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		//Creo una variable para almacenar el número que inserte el usuario
+		int n;
+		//Creo un escáner
+		Scanner reader = new Scanner(System.in);
+		//Pido al usuario un número hasta el que contar
+		System.out.println("Introduzca un número para contar desde 1 hasta ese:");
+		n = reader.nextInt();
+		//Creo un bucle for para contar de uno hasta el número introducido
+		for (int i = 1 ; i<=n ; i++) {
+			System.out.println(i);
+		}
+		//Cierro el escáner
+		reader.close();
+	}
+
+}
