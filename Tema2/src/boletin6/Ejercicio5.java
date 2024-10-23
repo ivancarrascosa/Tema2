c611fb70b42c521e590e83a660a931b9af62d9f4
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2024-10-23 10:01:05 +0200

    Actualizacion ejercicio 5

--- Tema2/src/boletin6/Ejercicio5.java
+++ Tema2/src/boletin6/Ejercicio5.java
@@ -6,9 +6,9 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// Creo una variable para almacenar el número que introduzca el usuario
-		int n;
+		long n;
 		// Creo una variable para almacenar la respuesta que le voy a dar al usuario
-		int res = 1;
+		long res = 1;
 		// Creo un escáner
 		Scanner reader = new Scanner(System.in);
 		// Pido al usuario un número para hacer el factorial
@@ -17,7 +17,7 @@ public class Ejercicio5 {
 		// Creo un for que dado el número que introduzca el usuario, multiplique desde
 		// el 1 a ese número, usaré el contador i en el for para ir multiplicando hasta
 		// llegar al número
-		for (int i = 1; i <= n; i++) {
+		for (int i = (int)n; i > 1; i--) {
 			res *= i;
 		}
 		System.out.println(n + "! = " + res);
